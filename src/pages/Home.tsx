b748e673866f0085d0936ec9309c6f3a2b6e0752
Misaianes <sairebautista@gmail.com>
2025-11-07 19:40:46 -0300

    Se puso imagenes de prueba en los productos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,9 @@
 import ProductCard from "../components/ProductCard";
 import "./Home.css";
 const products = [
-  { id: 1, name: "Pan casero", price: 1500, image: "/images/pan.jpg" },
-  { id: 2, name: "Facturas surtidas", price: 2000, image: "/images/facturas.jpg" },
-  { id: 3, name: "Medialunas", price: 2500, image: "/images/medialunas.jpg" },
+  { id: 1, name: "Frolitas tofi", price: 1500, image: "../assets/logo.png" },
+  { id: 2, name: "Facturas surtidas", price: 2000, image: "../assets/logo.png" },
+  { id: 3, name: "Medialunas", price: 2500, image: "../assets/logo.png" },
 ];
 
 export default function Home() {
