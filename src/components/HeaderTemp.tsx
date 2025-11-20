9dc55aaebaeea81abd91bd5342ee08acd8c484ee
Misaianes <sairebautista@gmail.com>
2025-11-20 10:16:57 -0300

    se cambio nombre del header a temporal por causa de un error de github que guardaba mal el archivo

--- src/components/HeaderTemp.tsx
+++ src/components/HeaderTemp.tsx
@@ -2,16 +2,17 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
 import "./Header.css";
-import logo from "../assets/logo.png"; 
+import logo from "../assets/logo.png";
 import { useAuth } from "../context/AuthContext";
 
 export default function Header() {
+  const { user, logout } = useAuth();
   const { cart } = useContext(CartContext);
   const totalItems = cart.reduce((acc, item) => acc + (item.quantity || 1), 0);
 
   return (
     <header className="header">
-       <div className="logo-container">
+      <div className="logo-container">
         <img src={logo} alt="Logo" className="logo" />
         <h1>El Faro Panaderia</h1>
       </div>
@@ -20,29 +21,18 @@ export default function Header() {
         <Link to="/carrito">Carrito {totalItems > 0 && `(${totalItems})`}</Link>
         <Link to="/checkout">Ir a pagar</Link>
         <div>
-        {user ? (
-          <button onClick={logout}>
-            Cerrar sesión ({user.displayName})
-          </button>
-        ) : (
-          <button onClick={() => document.dispatchEvent(new CustomEvent("openLogin"))}>
-            Iniciar sesión
-          </button>
-        )}
-      </div>
+          {user ? (
+            <button onClick={logout}>
+              Cerrar sesión ({user.displayName})
+            </button>
+          ) : (
+            <button onClick={() => document.dispatchEvent(new CustomEvent("openLogin"))}>
+              Iniciar sesión
+            </button>
+          )}
+        </div>
       </nav>
     </header>
   );
 }
 
-export default function Header() {
-  const { user, logout } = useAuth();
-
-  return (
-    <header className="header">
-      <h1>El Faro Panadería</h1>
-
-      <
-    </header>
-  );
-}
