b9505712996e3da30913106075e4c9addab5bd96
Misaianes <sairebautista@gmail.com>
2025-11-25 21:19:18 -0300

    Fix del auth provider que tiene que estar fuera de todos los Componentes

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,12 @@ import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+      <AuthProvider>
 
     <CartProvider>
-      <AuthProvider>
         <App />
-      </AuthProvider>
     </CartProvider>
+      </AuthProvider>
+
   </React.StrictMode>
 );
